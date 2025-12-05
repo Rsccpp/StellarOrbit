1e3f6352bf398cd7c25e44f9fc045b81b0ba741e
Rsccpp <rohitsinghsstc2812@gmail.com>
2025-12-06 00:43:13 +0530

    initial change

--- backend/app.py
+++ backend/app.py
@@ -202,4 +202,4 @@ def serve_static_files(filename):
 
 if __name__ == '__main__':
     print("🚀 StellarOrbit Backend Initialized...")
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
